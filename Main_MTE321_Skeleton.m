7f73424effaaff37d39e437064ca830d2876b585
Jeremy Phy <jeremyphy@gmail.com>
2022-04-01 17:09:42 -0400

    Correct misplaced main
    - moved code contents, main code was previously placed in 'get_direction_of_force_alpha'
    - added plot formatting for first chart

--- Main_MTE321_Skeleton.m
+++ Main_MTE321_Skeleton.m
@@ -15,6 +15,11 @@ BC = 20;
 
 % Initialize arrays to collect desired values as needed
 M12 = [];
+Cx_list = zeros(1, 360);
+Cy_list = zeros(1, 360);
+theta3_list = zeros(1, 360);
+dtheta3_list = zeros(1, 360);
+ddtheta3_list = zeros(1, 360);
 
 % Might want to use the loop to iterate through the values of theta to 
 % find the required deliverables for all angles:
@@ -22,16 +27,20 @@ M12 = [];
 % NOTE: The teaching team chose to use a loop to run through the several theta 
 %       values, and to use functuion calls to do the required operations. You 
 %       can choose not to use either. You may get the same results, but the code
-%       may be harder to look through or debug. Do whatever works best for you!         
-
+%       may be harder to look through or debug. Do whatever works best for you!  
+x = 0 : 1 : 360;
 for theta2 = 0:1:360
-
-    theta3 = atand((12-5*sind(theta2))/(4-5*cos(theta2)));
+    
+    theta3 = atan2d((12-5*sind(theta2)), (4-5*cosd(theta2)));
     dtheta3 = ((5*dtheta2)/20)*cosd(theta2 - theta3);
     ddtheta3 = -(5/20)*(dtheta2^2)*sind(theta2 - theta3);
 
-    Cx = 5*cosd(theta2) + 20*cosd(theta3);
-    Cy = 5*sind(theta2) + 20*sin(theta3);
+    theta3_list(theta2 + 1) = theta3;
+    dtheta3_list(theta2 + 1) = dtheta3;
+    ddtheta3_list(theta2 + 1) = ddtheta3;
+
+    Cx_list(theta2 + 1) = 5*cosd(theta2) + 20*cosd(theta3);
+    Cy_list(theta2 + 1) = 5*sind(theta2) + 20*sind(theta3);
 
     dCx = -5*dtheta2*sind(theta2) - 20*dtheta3*sind(theta3);
     dCy = 5*dtheta2*cosd(theta2) + 20*dtheta3*cosd(theta3);
@@ -40,7 +49,7 @@ for theta2 = 0:1:360
     ddCy = -5*(dtheta2^2)*sind(theta2) - 20*(ddtheta3^2)*sind(theta3) + 20*ddtheta3*cosd(theta3);
 
     dR3 = 5*dtheta2*sind(theta2 - theta3);
-    
+   
     % Enter the variables you want to calculated at every iteration
     % eg:
     
@@ -68,7 +77,16 @@ for theta2 = 0:1:360
     
 end
 
+% Part A requirements
 % Plot all desired deliverables:
+figure_num = 1;
+figure(figure_num);
+plot(x, dtheta3_list);
+title("Angular Velocity of Link 3 -vs- Angular Position of Link 2");
+axis([0 360 -10 10])
+grid on;
+xlabel('\theta_2  [degree]')
+ylabel('d\theta_3  [N-m]')
 
 %figure (2)
 %plot(theta2_list,M12)
@@ -76,9 +94,6 @@ end
 %title('\theta_2 vs M12')
 %xlabel('\theta_2   unit: degree')
 %ylabel('M12   unit: N-m')
-
-
-
 
 
 
